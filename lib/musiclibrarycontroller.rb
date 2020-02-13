b2736b9ca78c2b8f015f5bf5bef4a52134b6fa11
Chloe Foronda <caforonda103@yahoo.com>
2020-02-13 01:53:47 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -66,7 +66,6 @@ class MusicLibraryController
     end
   end
   def list_genres
-    #prints all genres in the music library in a numbered list (alphabetized by genre name)
     songs_sorted_by_genre = Genre.all.sort_by do |genre|
       genre.name
     end
