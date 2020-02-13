cdd540101b1b875cc9a9cb8d8e47e05e1d79bbf3
Chloe Foronda <caforonda103@yahoo.com>
2020-02-13 01:54:05 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -65,14 +65,7 @@ class MusicLibraryController
       end
     end
   end
-  def list_genres
-    songs_sorted_by_genre = Genre.all.sort_by do |genre|
-      genre.name
-    end
-    songs_sorted_by_genre.each.with_index(1) do |genre,index|
-      puts "#{index}. #{genre.name}"
-    end
-  end
+
   
   
   
