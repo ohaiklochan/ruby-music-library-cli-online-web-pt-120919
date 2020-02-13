d84360c6efedb50b49c0b2b50364b403afaab902
Chloe Foronda <caforonda103@yahoo.com>
2020-02-13 01:15:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,7 +59,10 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    info = self.filename
-    
-  
+    info = filename.split(" - ")
+    artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
+    genre = Genre.find_or_create_by_name(genre)
+    artist = Artist.find_or_create_by_name(artist)
+    new(name,artist,genre)
+  end
 end
