44256ecd5a82643a2e42115b0a081dcba0489c79
Chloe Foronda <caforonda103@yahoo.com>
2020-02-13 01:09:25 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -6,7 +6,7 @@ module Concerns::Findable
     self.all.detect {|song| song.name == name}
   end
 
-  def find_or_create_by_name
+  def find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
   
