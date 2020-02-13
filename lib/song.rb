e3361eff2c6ab2afc18d52b6ef3f1cc1219b4f64
Chloe Foronda <caforonda103@yahoo.com>
2020-02-13 01:15:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,8 @@ class Song
     self.find_by_name(name) || self.create(name)
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+    info = self.filename
     
   
 end
